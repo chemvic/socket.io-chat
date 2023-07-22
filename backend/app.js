e287501a76ee5786374e86d11ee4bca13f165b8b
chemvic <chemvic79@gmail.com>
2023-07-22 11:24:59 +0300

    back

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const {Server} = require('socket.io');
+const{createServer} = require('http');
+
+const httpServer = createServer();
+
+const io =new Server(httpServer, {
+    cors:{
+        origin :"*"
+    }
+});
+
+io.on('connection', (socket)=>{
+console.log('Привет');
+})
